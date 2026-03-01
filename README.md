ec822d36750f0ad9a42bb1a874d0434cf856ba22
MZandtheRaspberryPi <mzandtheraspberrypi@gmail.com>
2026-03-01 16:18:44 +0000

    readme updates

--- README.md
+++ README.md
@@ -63,7 +63,7 @@ mjpg_streamer -i "input_uvc.so -d /dev/video0 -r 1280x720 -f 10" -o "output_http
 ### timelapse
 
 ```
-sudo apt-get install ffmpeg
+sudo apt-get install -y ffmpeg python3-venv
 cd $HOME
 git clone https://github.com/MZandtheRaspberryPi/pi_home_assistant.git
 cd pi_home_assistant
@@ -71,3 +71,66 @@ python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 ```
+
+```
+sudo nano /etc/systemd/system/timelapse.service
+```
+
+```
+[Unit] 
+Description=Timelapse Service
+After=network-online.target 
+Wants=network-online.target systemd-networkd-wait-online.service 
+
+[Service] 
+ExecStart=/home/darsh/pi_home_assistant/venv/bin/python3 /home/darsh/pi_home_assistant/timelapse.py
+
+[Install] 
+WantedBy=multi-user.target 
+```
+
+```
+sudo systemctl start timelapse
+sudo systemctl enable timelapse
+sudo reboot
+```
+
+### copying timelapses and converting them
+
+The images are stored on the remote computer if using the python based client `timelapse.py`. They are in the `DATA_DIR` which by default is `/home/darsh/timelapse`. By default they will be kept for `FILE_DELETION_DAYS` which is set to `14` by default, then images and videos will be deleted to avoid filling up the drive.
+
+Connect to the computer, see the timelapses available
+```
+ssh darsh@10.0.0.110
+ls /home/darsh/timelapse/*.avi
+```
+
+Now note the name of the .avi file and on your computer that you wish to have the file on, copy it out using scp:
+```
+scp darsh@10.0.0.110:/home/darsh/timelapse/2026-03-01_06-00-01.avi .
+```
+
+On your output computer, use ffmpeg to convert it to an mp4. In testing, converion on the raspberry pi didn't work well though this could probably be resolved with more testing...
+```
+ffmpeg -i /home/darsh/timelapse/2026-03-01_06-00-01.avi -strict -2 /home/darsh/timelapse/2026-03-01_06-00-01.avi.mp4
+```
+
+### seeing camera settings
+
+This can be done through MJPEG Streamer through the control tab `http://10.0.0.110:8080/index.html`.
+
+Or, using v4l-utils.
+
+```
+sudo apt-get install v4l-utils
+v4l2-ctl -d /dev/video0 -l # see the controls available
+```
+
+Change a channel
+```
+v4l2-ctl -d /dev/video0 -c example_channel=0
+```
+
+### changing timelapse settings
+
+By default the python client can take one timelapse a day. It has a start time `START_TIME` set to 6:00 AM by default, and a duration, `DURATION_SECONDS` set to 2 hours by default. Between these times it takes a snapshot every `SNAPSHOT_INTERVAL` seconds, set to 5 by default. When it is past the end time, it processes the snapshots into a video (a `.avi` file). The `.avi` format is more legacy whereas `.mp4` is more widely used on the internet and what's app. Hence conversion of the video may be needed before sharing.
--- timelapse.py
+++ timelapse.py
@@ -1,28 +1,97 @@
 import datetime
+from enum import Enum
+import os
+import shutil
+import subprocess
+import time
+
 import requests
 
-IP_ADDRESS = "10.0.0.110"
+IP_ADDRESS = "localhost"
 PORT = 8080
-
+START_TIME = datetime.time(hour=6, minute=00, second=00)
+DURATION_SECONDS = 60*120 # 2 hours
+SNAPSHOT_INTERVAL = 5.0
+DATA_DIR = "/home/darsh/timelapse"
 # URL of the mjpg-streamer snapshot endpoint
 ENDPOINT = f"http://{IP_ADDRESS}:{PORT}/?action=snapshot"
+FILE_DELETION_DAYS = 14
+
+N_ZEROS_IMG_NAME = 6
+
+class Status(Enum):
+    WAIT = 0
+    TAKING_TIMELAPSE = 1
+
 
 def take_snapshot(output_filename='snapshot.jpg'):
-    try:
-        # Send GET request to capture image
-        response = requests.get(ENDPOINT, timeout=5)
+    response = requests.get(ENDPOINT, timeout=5)
+    response.raise_for_status()
+    with open(output_filename, 'wb') as f:
+        f.write(response.content)
+
+def make_video(filename_path: str, vid_dir: str, fps: float):
+    in_dir_pattern = os.path.join(vid_dir, f"*.jpg")
+    ffmpeg_cmd = ["ffmpeg", "-y", "-framerate", str(fps), "-pattern_type", "glob", "-i", f"{in_dir_pattern}", "-c:v", "mjpeg", filename_path]
+    res = subprocess.run(ffmpeg_cmd, shell=False, capture_output=True, timeout=240)
+    assert res.returncode == 0, f"{res}"
+
+
+def main():
+
+    status = Status.WAIT
+    timelapse_subdir = None
+    img_ctr = 0
+
+    while True:
+        cur_time = datetime.datetime.now()
+        timelapse_start = datetime.datetime.combine(datetime.date.today(), START_TIME)
+        timelapse_end = timelapse_start + datetime.timedelta(seconds=DURATION_SECONDS)
+        if status == Status.WAIT:
+            # clean up old files
+            stuff = os.listdir(DATA_DIR)
+            for f in stuff:
+                f_path = os.path.join(DATA_DIR, f)
+                mod_time = os.stat(f_path).st_mtime
+                file_time_sec = time.time() - mod_time
+                file_time_days = file_time_sec / (60 * 60 * 24)
+                if file_time_days > FILE_DELETION_DAYS:
+                    if os.path.isfile(f_path):
+                        os.remove(f_path)
+                    elif os.path.isdir(f_path):
+                        shutil.rmtree(f_path)
+
+            # check if time to do another timelapse
+            if cur_time > timelapse_start and cur_time < timelapse_end:
+                status = Status.TAKING_TIMELAPSE
+                continue
         
-        # Check if the request was successful
-        if response.status_code == 200:
-            # Write the binary image content to file
-            with open(output_filename, 'wb') as f:
-                f.write(response.content)
-            print(f"Snapshot saved to {output_filename}")
-        else:
-            print(f"Failed to get snapshot, status code: {response.status_code}")
-            
-    except Exception as e:
-        print(f"Error: {e}")
+        if status == Status.TAKING_TIMELAPSE:
+            # check if started, if not init variables
+            if timelapse_subdir is None:
+                timelapse_subdir = cur_time.strftime("%Y-%m-%d_%H-%M-%S")
+                timelapse_subdir = os.path.join(DATA_DIR, timelapse_subdir)
+                os.makedirs(timelapse_subdir, exist_ok=False)
+
+            # take photo
+            photo_name = f"my_img_{str(img_ctr).zfill(N_ZEROS_IMG_NAME)}" + ".jpg"
+            photo_name = os.path.join(timelapse_subdir, photo_name)
+            take_snapshot(photo_name)
+
+            img_ctr += 1
+
+            # check if done, if so make video and reset variables
+            if cur_time > timelapse_end:
+                make_video(timelapse_subdir + ".avi", timelapse_subdir, 30)
+                timelapse_subdir = None
+                img_ctr = 0
+                status = Status.WAIT
+
+        time.sleep(SNAPSHOT_INTERVAL)
+
+
+
+
 
 if __name__ == "__main__":
-    take_snapshot()
+    main()
